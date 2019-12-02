2506f2bea82d27395a259d4dd4247101334f4e91
Maximiliano Céspedes <maxicespedes7@hotmail.com>
2019-12-02 20:26:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const breakpoints = { tablet: 769, desktop:1024, fullhd: 1408}
 const mq = Object.keys(breakpoints).reduce((leftovers, bp) => (
   {
   ...leftovers,
-  bp: `@media (min-width: ${breakpoints[bp]}px)`
+  [bp]: `@media (min-width: ${breakpoints[bp]}px)`
 }), {});
